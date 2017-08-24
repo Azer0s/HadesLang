e7af315a90151e373f1229c34c3727d22110bcb5
Azer0s <sim17057@spengergasse.at>
2017-08-24 15:55:34 +0200

    Added debug

---
+++ Debug/HadesDebugger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debug
+{
+    public class HadesDebugger
+    {
+        /// <summary>
+        /// Lazy Store
+        /// </summary>
+        private static readonly Lazy<HadesDebugger> Lazy = new Lazy<HadesDebugger>(() => new HadesDebugger());
+        /// <summary>
+        /// Cache instance
+        /// </summary>
+        public static HadesDebugger EventManager => Lazy.Value;
+        /// <summary>
+        /// Method for interrupting the program
+        /// </summary>
+        public delegate bool Interrupt(int line, string varDump);
+        /// <summary>
+        /// Event to subscribe to
+        /// </summary>
+        public event Interrupt OnInterrupted;
+        /// <summary>
+        /// Prevents a default instance of the <see cref="HadesDebugger"/> class from being created.
+        /// </summary>
+        private HadesDebugger()
+        {
+        }
+    }
+}
