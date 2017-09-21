ced74ea4546a14fee20c00eec35efae68ec5425c
Azer0s <ari.simulevski@gmail.com>
2017-09-22 00:12:34 +0200

    Added project for rest library

--- file/GlobalSuppressions.cs
+++ file/GlobalSuppressions.cs
@@ -5,4 +5,5 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:file.Library.readAllLines~System.String")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:file.Library.readAllLines(System.String)~System.String")]
 
--- file/Library.cs
+++ file/Library.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using StringExtension;
+// ReSharper disable InconsistentNaming
 
 namespace file
 {
