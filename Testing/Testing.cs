3189b454431e4c8104c3fd72f4b5fbb9460938c3
Ariel Simulevski <sim17057@spengergasse.at>
2017-11-22 12:42:28 +0100

    Added getfields and tests for getfields, reworked some test methods

--- Testing/Testing.cs
+++ Testing/Testing.cs
@@ -12,35 +12,34 @@ namespace Testing
     [TestFixture]
     public class Testingcs
     {
-        private readonly Interpreter.Interpreter _interpreter = new Interpreter.Interpreter(new ConsoleOutput(), new ConsoleOutput());
+        private readonly Interpreter.Interpreter inter = new Interpreter.Interpreter(new ConsoleOutput(), new ConsoleOutput());
         private string prefix = "C:\\Users\\ariel\\workspace\\HadesLang\\HadesLang\\";
 
         [TearDown]
         public void TearDown()
         {
-            _interpreter.InterpretLine("dumpVars:all", new List<string> {"testing"}, null, true);
-            _interpreter.InterpretLine("uload:all", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("dumpVars:all", new List<string> { "testing" }, null, true);
+            inter.InterpretLine("dumpVars:all", new List<string> {"testing"}, null, true);
+            inter.InterpretLine("uload:all", new List<string>{"testing"}, null);
+            inter.InterpretLine("dumpVars:all", new List<string> { "testing" }, null, true);
         }
 
         [Test]
         public void AddTest()
         {
-            Assert.AreEqual("15", _interpreter.InterpretLine("10+5", new List<string>{"testing"}, null));
+            Assert.AreEqual("15", inter.InterpretLine("10+5", new List<string>{"testing"}, null));
         }
 
         [Test]
         public void PipelineTest()
         {
-            _interpreter.InterpretLine($"with '{prefix}fibrec.hades' as a", new List<string> { "testing" }, null);
-            Assert.AreEqual("'34'", _interpreter.InterpretLine("$a->fib:[9] |> out:[??] |> out:[??]", new List<string> { "testing" }, null));
-            _interpreter.InterpretLine($"uload:a", new List<string> { "testing" }, null);
+            inter.InterpretLine($"with '{prefix}fibrec.hades' as a", new List<string> { "testing" }, null);
+            Assert.AreEqual("'34'", inter.InterpretLine("$a->fib:[9] |> out:[??] |> out:[??]", new List<string> { "testing" }, null));
         }
 
         [Test]
         public void ConcatTest()
         {
-            Assert.AreEqual("'Hello world'",_interpreter.InterpretLine("'Hello' + ' ' + 'world'", new List<string>{"testing"}, null));
+            Assert.AreEqual("'Hello world'",inter.InterpretLine("'Hello' + ' ' + 'world'", new List<string>{"testing"}, null));
         }
 
         [Test]
@@ -50,16 +49,15 @@ namespace Testing
         [TestCase("d", "bit", "true")]
         public void CreateVar(string name, string dataType, string value)
         {
-            _interpreter.InterpretLine($"{name} as {dataType} closed = {value}", new List<string>{"testing"}, null);
-            Assert.AreEqual(value, _interpreter.InterpretLine(name, new List<string>{"testing"}, null));
-            _interpreter.InterpretLine($"uload:{name}", new List<string>{"testing"}, null);
+            inter.InterpretLine($"{name} as {dataType} closed = {value}", new List<string>{"testing"}, null);
+            Assert.AreEqual(value, inter.InterpretLine(name, new List<string>{"testing"}, null));
         }
 
         [Test]
         public void FibTest()
         {
-            _interpreter.InterpretLine($"with '{prefix}fib.hades' as a", new List<string> {"testing"}, null);
-            Assert.AreEqual("5",_interpreter.InterpretLine("$a->test:[]", new List<string> { "testing" }, null));
+            inter.InterpretLine($"with '{prefix}fib.hades' as a", new List<string> {"testing"}, null);
+            Assert.AreEqual("5",inter.InterpretLine("$a->test:[]", new List<string> { "testing" }, null));
         }
 
         [Test]
@@ -68,16 +66,16 @@ namespace Testing
         [TestCase("Hello world", "325")]
         public void GuardTest(string expected, string value)
         {
-            _interpreter.InterpretLine($"with '{prefix}fib.hades' as a", new List<string> { "testing" }, null);
-            Assert.AreEqual(expected, _interpreter.InterpretLine($"$a->t1:[{value}]", new List<string> { "testing" }, null));
+            inter.InterpretLine($"with '{prefix}fib.hades' as a", new List<string> { "testing" }, null);
+            Assert.AreEqual(expected, inter.InterpretLine($"$a->t1:[{value}]", new List<string> { "testing" }, null));
         }
 
         [Test]
         public void CallTest()
         {
-            _interpreter.InterpretLine($"with \'{prefix}iterate.hades\' as a",new List<string>{"testing"},null);
-            _interpreter.InterpretLine($"with \'{prefix}fibrec.hades\' as b",new List<string>{"testing"},null);
-            Assert.AreEqual("55",_interpreter.InterpretLine("$a->test:[$b,10]", new List<string>{"testing"}, null));
+            inter.InterpretLine($"with \'{prefix}iterate.hades\' as a",new List<string>{"testing"},null);
+            inter.InterpretLine($"with \'{prefix}fibrec.hades\' as b",new List<string>{"testing"},null);
+            Assert.AreEqual("55",inter.InterpretLine("$a->test:[$b,10]", new List<string>{"testing"}, null));
         }
 
         [Test]
@@ -87,48 +85,42 @@ namespace Testing
         [TestCase("d", "dec", "{1.3,0.9}", "1", "0.9")]
         public void ArrayTest(string name, string dataType, string values, string postion, string expected)
         {
-            _interpreter.InterpretLine($"{name} as {dataType}[*] closed = {values}", new List<string>{"testing"}, null);
-            Assert.AreEqual(expected, _interpreter.InterpretLine($"{name}[{postion}]", new List<string>{"testing"}, null));
-            _interpreter.InterpretLine($"uload:{name}", new List<string>{"testing"}, null);
+            inter.InterpretLine($"{name} as {dataType}[*] closed = {values}", new List<string>{"testing"}, null);
+            Assert.AreEqual(expected, inter.InterpretLine($"{name}[{postion}]", new List<string>{"testing"}, null));
         }
 
         [Test]
         public void ArrayAssign()
         {
-            _interpreter.InterpretLine("a as num[*] closed = {1,2,3,4,5}", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("b as num[*] closed", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("b = a", new List<string>{"testing"}, null);
-            Assert.AreEqual("{1,2,3,4,5}", _interpreter.InterpretLine("b", new List<string>{"testing"}, null));
-            _interpreter.InterpretLine("uload:a", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("uload:b", new List<string>{"testing"}, null);
+            inter.InterpretLine("a as num[*] closed = {1,2,3,4,5}", new List<string>{"testing"}, null);
+            inter.InterpretLine("b as num[*] closed", new List<string>{"testing"}, null);
+            inter.InterpretLine("b = a", new List<string>{"testing"}, null);
+            Assert.AreEqual("{1,2,3,4,5}", inter.InterpretLine("b", new List<string>{"testing"}, null));
+
         }
 
         [Test]
         public void InterpreterForcing()
         {
-            _interpreter.InterpretLine("a as word closed = 'out:b'", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("b as word closed = 'Hello world'", new List<string>{"testing"}, null);
-            Assert.AreEqual("'Hello world'", _interpreter.InterpretLine("#a", new List<string>{"testing"}, null));
-            _interpreter.InterpretLine("uload:a", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("uload:b", new List<string>{"testing"}, null);
+            inter.InterpretLine("a as word closed = 'out:b'", new List<string>{"testing"}, null);
+            inter.InterpretLine("b as word closed = 'Hello world'", new List<string>{"testing"}, null);
+            Assert.AreEqual("'Hello world'", inter.InterpretLine("#a", new List<string>{"testing"}, null));
+
         }
 
         [Test]
         public void ForceAssign()
         {
-            _interpreter.InterpretLine("a as word closed = 'out:b'", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("b as word closed = 'Hello world'", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("c as word closed = #a", new List<string>{"testing"}, null);
-            Assert.AreEqual("'Hello world'", _interpreter.InterpretLine("c", new List<string>{"testing"}, null));
-            _interpreter.InterpretLine("uload:a", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("uload:b", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("uload:c", new List<string>{"testing"}, null);
+            inter.InterpretLine("a as word closed = 'out:b'", new List<string>{"testing"}, null);
+            inter.InterpretLine("b as word closed = 'Hello world'", new List<string>{"testing"}, null);
+            inter.InterpretLine("c as word closed = #a", new List<string>{"testing"}, null);
+            Assert.AreEqual("'Hello world'", inter.InterpretLine("c", new List<string>{"testing"}, null));
         }
 
         [Test]
         public void FileTest()
         {
-            Assert.AreEqual("23",_interpreter.InterpretLine($"with \'{prefix}fibrec.hades\'", new List<string>{"testing"},null));
+            Assert.AreEqual("23",inter.InterpretLine($"with \'{prefix}fibrec.hades\'", new List<string>{"testing"},null));
         }
 
         [Test]
@@ -137,7 +129,7 @@ namespace Testing
         [TestCase("'DEC'", "3.2")]
         public void RawTest(string expected, string data)
         {
-            Assert.AreEqual(expected, _interpreter.InterpretLine($"out:[dtype:[raw:['{data}']]]", new List<string>{"testing"}, null));
+            Assert.AreEqual(expected, inter.InterpretLine($"out:[dtype:[raw:['{data}']]]", new List<string>{"testing"}, null));
         }
 
         [Test]
@@ -146,7 +138,7 @@ namespace Testing
         [TestCase("false", "(true and false) or (true and false)")]
         public void BoolTests(string expected, string calc)
         {
-            Assert.AreEqual(expected, _interpreter.InterpretLine(calc, new List<string>{"testing"}, null));
+            Assert.AreEqual(expected, inter.InterpretLine(calc, new List<string>{"testing"}, null));
         }
 
         [Test]
@@ -155,20 +147,28 @@ namespace Testing
         [TestCase("DEC", "0.9")]
         public void TypeTest(string expected, string calc)
         {
-            Assert.AreEqual(expected, _interpreter.InterpretLine($"dtype:{calc}", new List<string>{"testing"}, null));
+            Assert.AreEqual(expected, inter.InterpretLine($"dtype:{calc}", new List<string>{"testing"}, null));
         }
 
         [Test]
         public void VarAssign()
         {
-            _interpreter.InterpretLine("a as num closed = 1", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("b as num closed = 2", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("a = $a + $b", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("b = $a + $b", new List<string>{"testing"}, null);
-            Assert.AreEqual("5", _interpreter.InterpretLine("b", new List<string>{"testing"}, null));
-            _interpreter.InterpretLine("uload:a", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("uload:b", new List<string>{"testing"}, null);
-            _interpreter.InterpretLine("uload:c", new List<string>{"testing"}, null);
+            inter.InterpretLine("a as num closed = 1", new List<string>{"testing"}, null);
+            inter.InterpretLine("b as num closed = 2", new List<string>{"testing"}, null);
+            inter.InterpretLine("a = $a + $b", new List<string>{"testing"}, null);
+            inter.InterpretLine("b = $a + $b", new List<string>{"testing"}, null);
+            Assert.AreEqual("5", inter.InterpretLine("b", new List<string>{"testing"}, null));
+        }
+
+        [Test]
+        public void ReflectionTest()
+        {
+            inter.InterpretLine("a as num", new List<string> {"testing"}, null);
+            inter.InterpretLine("b as bit", new List<string> {"testing"}, null);
+            inter.InterpretLine("c as word = 'HELLO'", new List<string> { "testing" }, null);
+            inter.InterpretLine("d as word[*] = getfields:[]", new List<string> {"testing"}, null);
+            inter.InterpretLine("f as word = #d[2]", new List<string> {"testing"}, null);
+            Assert.AreEqual("'HELLO'", inter.InterpretLine("out:f", new List<string> { "testing" }, null));
         }
 
         [Test]
@@ -176,7 +176,7 @@ namespace Testing
         [TestCase("out:[dtype:12]","'NUM'")]
         public void StringCompTest(string a, string b)
         {
-            Assert.AreEqual("true",_interpreter.InterpretLine($"{a} is {b}", new List<string>{"testing"}, null));
+            Assert.AreEqual("true",inter.InterpretLine($"{a} is {b}", new List<string>{"testing"}, null));
         }
     }
 }
