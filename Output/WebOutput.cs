3a08c7c7c21971ce800a5dd844d73c0dd5ca6589
Ariel Simulevski <ariel.simulevski@gmail.com>
2018-04-24 10:02:34 +0200

    Added weboutput

---
+++ Output/WebOutput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output
+{
+    class WebOutput : IScriptOutput
+    {
+        public StringBuilder Output;
+
+        public WebOutput()
+        {
+            Output = new StringBuilder("<!DOCTYPE html>");
+        }
+
+        public void Write(string input)
+        {
+            Output.Append(input);
+        }
+
+        public void WriteLine(string input)
+        {
+            Output.AppendLine(input);
+        }
+
+        public void Clear()
+        {
+            Output.Clear();
+        }
+
+        public string ReadLine()
+        {
+            return "";
+        }
+    }
+}
