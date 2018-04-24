25180fa0480ed7ee6e55727ad47009d28ed4d6ae
Ariel Simulevski <ariel.simulevski@gmail.com>
2018-04-24 20:12:43 +0200

    Added startup logo

--- Interpreter/ShellInterpreter.cs
+++ Interpreter/ShellInterpreter.cs
@@ -1,25 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using Output;
 using Variables;
 using static System.String;
+using Console = Colorful.Console;
 
 namespace Interpreter
 {
     public class ShellInterpreter
     {
+        private static readonly Color BLUE = Color.FromArgb(240, 6, 153);
+        private static readonly Color YELLOW = Color.FromArgb(247, 208, 2);
+        private static readonly Color PURPLE = Color.FromArgb(69, 78, 158);
+        private static readonly Color RED = Color.FromArgb(191, 26, 47);
+        private static readonly Color GREEN = Color.FromArgb(1, 142, 66);
+
         public ShellInterpreter(string path = null)
         {
-            var interpreter = new Interpreter(new ConsoleOutput(),new ConsoleOutput());
+            Console.Clear();
+            Console.WriteAscii("HadesLang",BLUE);
+            var interpreter = new Interpreter(new ConsoleOutput(), new ConsoleOutput());
+            Console.WriteLine();
+            Console.WriteLine("\t======================================",YELLOW);
+            Console.Write("\t||", YELLOW);
+            Console.Write("           Hades - v0.4           ", PURPLE);
+            Console.WriteLine("||",YELLOW);
+            Console.WriteLine("\t======================================", YELLOW);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("\t\t      Press key...", GREEN);
+            Console.ReadKey();
+            Console.Clear();
+
             interpreter.RegisterFunction(new Function("print", a =>
             {
                 a.ToList().ForEach(Console.WriteLine);
                 return "";
             }));
             Evaluator.AliasManager.Add("clear","cls");
-
-            interpreter.RegisterFunction(new Function("helloworld", a => "Hello world"));
 
             if (path != null)
             {
