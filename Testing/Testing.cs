55c27249f1f27df06408bc21bc2616d75e4efee5
Ariel Simulevski <ariel.simulevski@gmail.com>
2018-04-24 18:37:43 +0200

    Added debug project

--- Testing/Testing.cs
+++ Testing/Testing.cs
@@ -1,10 +1,9 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework.Internal;
+using NUnit.Framework;
 using Output;
 
 namespace Testing
