140da5d358850db21b0e7872017c02a33a91d660
Ariel Simulevski <ariel.simulevski@gmail.com>
2018-04-26 19:47:12 +0200

    Splited startup from main function

--- HadesWeb/Program.cs
+++ HadesWeb/Program.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using HadesWeb.Helper;
-using Output;
-using ServiceStack.Redis;
-using Variables;
+﻿using System.Threading;
 using Console = Colorful.Console;
 using static HadesWeb.Util.Log;
 // ReSharper disable InconsistentNaming
@@ -18,48 +9,31 @@ namespace HadesWeb
     {
         public static void Main(string[] args)
         {
+            Title();
+            var startup = new Startup(args);
+            startup.Start();
+
+            while (true)
+            {
+                Console.ReadKey();
+            }
+            // ReSharper disable once FunctionNeverReturns
+        }
+
+        private static void Title()
+        {
             Console.Clear();
             Console.Title = "HadesWeb Server";
-
             var name = string.Empty;
             foreach (var c in "HadesWeb Server")
             {
                 name += c;
                 Console.Clear();
-                Console.WriteAscii(name,Blue);
+                Console.WriteAscii(name, Blue);
                 Thread.Sleep(70);
             }
 
             Info("Initializing Hades Interpreter...");
-
-            if (args.Length == 0)
-            {
-                var cfg = ConfigHelper.BuildConfig("config.hd");
-                var server = new Server.Server(cfg.Address,cfg.Port,cfg.RoutingEnabled,cfg.Browser,cfg.Interpreter,cfg.Routes,cfg.Forward);
-                server.Start();
-            }
-            else
-            {
-                var configs = new List<ConfigHelper>();
-                
-                foreach (var s in args)
-                {
-                    configs.Add(ConfigHelper.BuildConfig(s));
-                    ConfigHelper.Reset();
-                }
-
-                foreach (var cfg in configs)
-                {
-                    var server = new Server.Server(cfg.Address,cfg.Port,cfg.RoutingEnabled,cfg.Browser,cfg.Interpreter,cfg.Routes,cfg.Forward);
-                    Task.Run(() =>
-                    {
-                        server.Start();
-                    });
-                }
-            }
-
-            Console.ReadKey();
-            // ReSharper disable once FunctionNeverReturns
         }
     }
 }
---
+++ HadesWeb/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HadesWeb.Helper;
+
+namespace HadesWeb
+{
+    class Startup
+    {
+        private readonly string[] _args;
+
+        public Startup(string[] args)
+        {
+            _args = args;
+        }
+
+        public void Start()
+        {
+            var configs = new List<ConfigHelper>();
+
+            if (_args.Length == 0)
+            {
+                configs.Add(ConfigHelper.BuildConfig("config.hd"));
+            }
+            else
+            {
+                foreach (var s in _args)
+                {
+                    configs.Add(ConfigHelper.BuildConfig(s));
+                    ConfigHelper.Reset();
+                }
+            }
+
+            foreach (var cfg in configs)
+            {
+                var server = new Server.Server(cfg.Address, cfg.Port, cfg.RoutingEnabled, cfg.Browser, cfg.Interpreter, cfg.Routes, cfg.Forward);
+                Task.Run(() =>
+                {
+                    server.Start();
+                });
+            }
+        }
+    }
+}
