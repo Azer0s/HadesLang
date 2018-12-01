4c58e35a7f1f503d89090f67780c4ea3263abd77
Unknown <sim17057@spengergasse.at>
2018-12-01 15:40:39 +0100

    Added base for tools

---
+++ src/Hades.Core/Tools/ProjectInitializer.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Hades.Core.Tools
+{
+    public static class ProjectInitializer
+    {
+        private static Regex _projectName = new Regex("[^\\.]");
+        
+        public static int Run(List<string> args)
+        {
+            System.Os
+            return 0;
+        }
+    }
+}
